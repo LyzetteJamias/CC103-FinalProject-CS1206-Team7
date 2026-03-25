0f73ed30d280ce65b621e0a9fe6ffe82d7cb8562
LyzetteJamias <jamiaslyzette@gmail.com>
2026-03-25 21:08:46 +0800

    first commit

---
+++ CC103-FinalProject-CS1206-Team7/.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "Being afraid isn't a bad thing. It's my fear that brought me this far." << endl;
+cout << "- Eisen";
+
+return 0;
+
+}
